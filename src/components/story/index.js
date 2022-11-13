5d04265176d00ffdeb71e48fc9283c05881191fe
andrew nefsky <anefsky@gmail.com>
2022-11-13 10:29:38 -0500

    cleanup

--- src/components/story/index.js
+++ src/components/story/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Wrapper, Entry, StoryImg, StoryTitle, StoryPara } from "./styles";
 
-const Story = (props) => {
+const Story = ({story}) => {
   return (
-    <Wrapper href={props.story.short_url}>
+    <Wrapper href={story.short_url}>
       <Entry>
         <StoryImg
           src={
-            props.story.multimedia
-              ? props.story.multimedia[1].url
+            story.multimedia
+              ? story.multimedia[1].url
               : "/img/no-image.png"
           }
           alt="images"
         />
         <div>
-          <StoryTitle>{props.story.title}</StoryTitle>
-          <StoryPara>{props.story.abstract}</StoryPara>
+          <StoryTitle>{story.title}</StoryTitle>
+          <StoryPara>{story.abstract}</StoryPara>
         </div>
       </Entry>
     </Wrapper>
